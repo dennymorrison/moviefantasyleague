e3ec04718017ceba6eede93b4c39fd5def00ef82
Denny <dennymorrison@users.noreply.github.com>
2014-07-11 12:42:40 -0400

    MFLBaseController changes.

--- MovieFantasyLeague/MFL.Web/Controllers/MFLBaseController.cs
+++ MovieFantasyLeague/MFL.Web/Controllers/MFLBaseController.cs
@@ -8,11 +8,22 @@ using System.Web.Mvc;
 
 namespace MFL.Web.Controllers
 {
-
-#if !DEBUG
-      [RequireHttps]  
-#endif
     public class MFLBaseController : Controller
-    {        
+    {
+        protected override IAsyncResult BeginExecute(System.Web.Routing.RequestContext requestContext, AsyncCallback callback, object state)
+        {
+#if !DEBUG
+            if(!HttpContext.Request.IsSecureConnection)
+            {
+                Response.Redirect(Request.Url.AbsoluteUri.Replace("http://", "https://"), true);                
+            }
+            else
+            {
+                base.BeginExecute(requestContext, callback, state);
+            }
+#else
+            return base.BeginExecute(requestContext, callback, state);
+#endif
+        }
 	}
 }
